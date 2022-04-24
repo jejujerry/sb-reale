604e0615f43b9c94632fda0623c4d2f6ac3fd18e
jejujerry <jejujerry@gmail.com>
2022-04-24 15:00:03 +0900

    Initial commit

---
+++ src/main/java/com/jejujerry/guestbook/service/GuestbookService.java
@@ -0,0 +1,45 @@
+package com.jejujerry.guestbook.service;
+
+import com.jejujerry.guestbook.dto.GuestbookDTO;
+import com.jejujerry.guestbook.entity.Guestbook;
+import com.jejujerry.guestbook.dto.PageRequestDTO;
+import com.jejujerry.guestbook.dto.PageResultDTO;
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+
+    GuestbookDTO read(Long gno);
+
+    void modify(GuestbookDTO dto);
+
+    void remove(Long gno);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+
+        return entity;
+    }
+
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+
+        GuestbookDTO dto  = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
+    }
+
+}
---
+++ src/test/java/com/jejujerry/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,49 @@
+package com.jejujerry.guestbook.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.jejujerry.guestbook.dto.GuestbookDTO;
+import com.jejujerry.guestbook.entity.Guestbook;
+import com.jejujerry.guestbook.dto.PageRequestDTO;
+import com.jejujerry.guestbook.dto.PageResultDTO;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister() {
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(service.register(guestbookDTO));
+    }
+
+
+    @Test
+    public void testList(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("-------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
+}
